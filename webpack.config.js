9f76ce536afa225a66a3b462f8fa4169cda48c79
fergmac <f.macconnell@gmail.com>
2017-02-27 09:41:32 -0800

    create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    entry: './app/app.jsx',
+    output: {
+        path: __dirname,
+        filename: './public/bundle.js'
+    },
+    resolve: {
+        //gives you path to file you are in
+        root: __dirname,
+        alias: {
+        },
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0']
+                },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+            }
+        ]
+    }
+};
+
+//this is so we can use babe, react and jsx
